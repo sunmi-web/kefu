8cdf7e7cc6a9505df4bf2947260254d651437d7d
guoyongjie <guoyongjie@sunmi.com>
2021-03-04 14:33:03 +0800

    feat: 默认创建实例

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ const Kefu = {
 window.Kefu = Kefu;
 window.__kefu__ = window.Kefu;
 
+window.addEventListener('load', () => {
+  if (!document.getElementsByTagName('sunmi-kefu')[0]) {
+    const sunmiKefu = document.createElement('sunmi-kefu');
+    document.body.appendChild(sunmiKefu);
+  }
+});
+
 declare global {
   export interface Window {
     Kefu: typeof Kefu;
